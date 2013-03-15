994135b8d5be6c102f28a105ebafc1c14582f534
Scott90 <my@fake.email>
2013-03-15 16:17:17 -0500

    Initial Commit

---
+++ SBSegmentedViewController/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SBSegmentedViewController
+//
+//  Created by Scott Berrevoets on 3/15/13.
+//  Copyright (c) 2013 Scotty Doesn't Code. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
