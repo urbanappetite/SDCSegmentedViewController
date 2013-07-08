856de8d7d5f506fda32a353bd63cea69fffade7e
Scott Berrevoets <Scott@hrh-sber-osx-ws.homerunholdings.com>
2013-07-08 16:43:57 -0500

    Fix small spacing issue

--- SBSegmentedViewController/SBSegmentedViewController.m
+++ SBSegmentedViewController/SBSegmentedViewController.m
@@ -100,7 +100,7 @@
 }
 
 - (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
+	[super viewWillAppear:animated];
     
 	if (!self.hasAppeared) {
         self.hasAppeared = YES;
