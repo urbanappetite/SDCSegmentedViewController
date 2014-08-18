e52cb4eabc5409f8cbc8058dfc322d9fea7c3302
Scott Berrevoets <s.berrevoets@me.com>
2014-08-17 18:41:52 -0700

    Create tests for position checking

--- SDCSegmentedViewController Tests/SDCSegmentedViewController_Tests.m
+++ SDCSegmentedViewController Tests/SDCSegmentedViewController_Tests.m
@@ -14,6 +14,7 @@ static NSString *const SDCViewControllerDefaultTitle1 = @"View controller 1";
 static NSString *const SDCViewControllerDefaultTitle2 = @"View controller 2";
 
 @interface SDCSegmentedViewController_Tests : XCTestCase
+@property (nonatomic, strong) SDCSegmentedViewController *segmentedController;
 @property (nonatomic, strong) NSArray *viewControllers;
 @end
 
@@ -27,11 +28,11 @@ static NSString *const SDCViewControllerDefaultTitle2 = @"View controller 2";
     viewController2.title = SDCViewControllerDefaultTitle2;
     
     self.viewControllers = @[viewController1, viewController2];
+    self.segmentedController = [[SDCSegmentedViewController alloc] initWithViewControllers:self.viewControllers];
 }
 
 - (void)testInitializingWithoutTitlesUsesViewControllerTitles {
-    SDCSegmentedViewController *segmentedController = [[SDCSegmentedViewController alloc] initWithViewControllers:self.viewControllers];
-    UISegmentedControl *segmentedControl = segmentedController.segmentedControl;
+    UISegmentedControl *segmentedControl = self.segmentedController.segmentedControl;
     
     XCTAssertEqualObjects([segmentedControl titleForSegmentAtIndex:0], SDCViewControllerDefaultTitle1, @"");
     XCTAssertEqualObjects([segmentedControl titleForSegmentAtIndex:1], SDCViewControllerDefaultTitle2, @"");
@@ -41,13 +42,29 @@ static NSString *const SDCViewControllerDefaultTitle2 = @"View controller 2";
     NSString *userProvidedTitle1 = @"User title 1";
     NSString *userProvidedTitle2 = @"User title 2";
     
-    SDCSegmentedViewController *segmentedController = [[SDCSegmentedViewController alloc] initWithViewControllers:self.viewControllers
-                                                                                                           titles:@[userProvidedTitle1, userProvidedTitle2]];
-    
-    UISegmentedControl *segmentedControl = segmentedController.segmentedControl;
+    self.segmentedController = [[SDCSegmentedViewController alloc] initWithViewControllers:self.viewControllers
+                                                                                    titles:@[userProvidedTitle1, userProvidedTitle2]];
+    UISegmentedControl *segmentedControl = self.segmentedController.segmentedControl;
 
     XCTAssertEqualObjects([segmentedControl titleForSegmentAtIndex:0], userProvidedTitle1, @"");
     XCTAssertEqualObjects([segmentedControl titleForSegmentAtIndex:1], userProvidedTitle2, @"");
+}
+
+- (void)testSegmentedControlShowsInNavigationBarWhenUserWantsItTo {
+    self.segmentedController.position = SDCSegmentedViewControllerControlPositionNavigationBar;
+    
+    XCTAssertEqualObjects(self.segmentedController.segmentedControl, self.segmentedController.navigationItem.titleView, @"");
+}
+
+- (void)testSegmentedControlShowsCenteredInToolbarWhenUserWantsItTo {
+    self.segmentedController.position = SDCSegmentedViewControllerControlPositionToolbar;
+ 
+    XCTAssertNoThrow(self.segmentedController.toolbarItems[0], @"");
+    XCTAssertNoThrow(self.segmentedController.toolbarItems[2], @"");
+    
+    UIBarButtonItem *segmentedControlItem;
+    XCTAssertNoThrow(segmentedControlItem = self.segmentedController.toolbarItems[1], @"");
+    XCTAssertEqualObjects(segmentedControlItem.customView, self.segmentedController.segmentedControl, @"");
 }
 
 @end
